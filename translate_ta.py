77db82ec07e57ea113c1d144f219b7e103c5a496
vikas <vikasdudi22@gmail.com>
2023-06-01 08:20:47 +0530

    final commit

---
+++ translate_ta.py
@@ -0,0 +1,88 @@
+import numpy as np
+from keras.models import Model
+from keras.layers import Input, LSTM, Dense, TimeDistributed, Embedding, Bidirectional
+
+input_token_index = np.load('models/tamil/input_token_index.npy', allow_pickle=True).item()
+target_token_index = np.load('models/tamil/target_token_index.npy', allow_pickle=True).item()
+reverse_input_char_index = np.load('models/tamil/reverse_input_char_index.npy', allow_pickle=True).item()
+reverse_target_char_index = np.load('models/tamil/reverse_target_char_index.npy', allow_pickle=True).item()
+num_encoder_tokens = 33708
+num_decoder_tokens = 40803
+
+latent_dim = 300
+# Encoder
+encoder_inputs = Input(shape=(None,))
+enc_emb =  Embedding(num_encoder_tokens, latent_dim, mask_zero = True)(encoder_inputs)
+encoder_lstm = LSTM(latent_dim, return_state=True)
+encoder_outputs, state_h, state_c = encoder_lstm(enc_emb)
+# We discard `encoder_outputs` and only keep the states.
+encoder_states = [state_h, state_c]
+
+# Set up the decoder, using `encoder_states` as initial state.
+decoder_inputs = Input(shape=(None,))
+dec_emb_layer = Embedding(num_decoder_tokens, latent_dim, mask_zero = True)
+dec_emb = dec_emb_layer(decoder_inputs)
+decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
+decoder_outputs, _, _ = decoder_lstm(dec_emb, initial_state=encoder_states)
+decoder_dense = Dense(num_decoder_tokens, activation='softmax')
+decoder_outputs = decoder_dense(decoder_outputs)
+
+model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+model.load_weights('models/tamil/eng-to-tamil.h5')
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+encoder_model = Model(encoder_inputs, encoder_states)
+
+# Decoder setup
+decoder_state_input_h = Input(shape=(latent_dim,))
+decoder_state_input_c = Input(shape=(latent_dim,))
+decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+decoder_outputs2, state_h2, state_c2 = decoder_lstm(dec_emb, initial_state=decoder_states_inputs)
+decoder_states2 = [state_h2, state_c2]
+decoder_outputs2 = decoder_dense(decoder_outputs2)
+
+# Final decoder model
+decoder_model = Model(
+    [decoder_inputs] + decoder_states_inputs,
+    [decoder_outputs2] + decoder_states2)
+
+def decode_sequence_tamil(input_text):
+    input_seq = np.zeros((1,20))
+    for idx, char in enumerate(input_text.split()):
+        if char in input_token_index:
+            idx2 = input_token_index[char]
+            input_seq[0, idx] = idx2
+    print(input_seq)
+    # Encode the input as state vectors.
+    states_value = encoder_model.predict(input_seq)
+    # Generate empty target sequence of length 1.
+    target_seq = np.zeros((1,1))
+    # Populate the first character of target sequence with the start character.
+    target_seq[0, 0] = target_token_index['START_']
+
+    # Sampling loop for a batch of sequences
+    # (to simplify, here we assume a batch of size 1).
+    stop_condition = False
+    decoded_sentence = ''
+    while not stop_condition:
+        output_tokens, h, c = decoder_model.predict([target_seq] + states_value)
+
+        # Sample a token
+        sampled_token_index = np.argmax(output_tokens[0, -1, :])
+        sampled_char = reverse_target_char_index[sampled_token_index]
+        decoded_sentence += ' '+sampled_char
+
+        # Exit condition: either hit max length
+        # or find stop character.
+        if (sampled_char == '_END' or
+           len(decoded_sentence) > 50):
+            stop_condition = True
+
+        # Update the target sequence (of length 1).
+        target_seq = np.zeros((1,1))
+        target_seq[0, 0] = sampled_token_index
+
+        # Update states
+        states_value = [h, c]
+
+    return decoded_sentence[:-4]
